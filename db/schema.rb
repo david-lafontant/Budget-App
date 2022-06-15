cf720c625d6026bf22efdb95fd7871316b82cabe
david-lafontant <lafontant.david@gmail.com>
2022-06-15 12:23:00 -0600

    Add foreign keys to tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_15_180844) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_15_182124) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_15_180844) do
     t.decimal "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
+    t.index ["group_id"], name: "index_expenses_on_group_id"
+    t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -26,6 +30,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_15_180844) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,4 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_15_180844) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "expenses", "groups"
+  add_foreign_key "expenses", "users"
+  add_foreign_key "groups", "users"
 end
