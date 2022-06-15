5362f063162f0331bf658b269559110f9726e171
david-lafontant <lafontant.david@gmail.com>
2022-06-15 12:17:50 -0600

    Update validations in models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
     belongs_to :user
-    validates :name, presence: true
+    has_many :expenditures
+  
+    validates :name, presence: true,  length: { minimum: 3, maximum: 100 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Must be a valid email address'}
   validates :name, presence: true, length: { minimum: 3, maximum: 100 }
+  
   has_many :groups, dependent: :destroy
+  has_many :expenditures, dependent: :destroy
 end
