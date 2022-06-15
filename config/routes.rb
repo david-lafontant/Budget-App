1e0bc25a4258ffd6e74f7cd14b8b73c2e68e484a
david-lafontant <lafontant.david@gmail.com>
2022-06-15 14:45:04 -0600

    Create Expenses controller abd view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'expenses/index'
+  get 'expenses/show'
+  get 'expenses/new'
+  get 'expenses/edit'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
