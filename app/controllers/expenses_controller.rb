8280e13911758aecf7c3658d2f7b1a9ed978f225
david-lafontant <lafontant.david@gmail.com>
2022-06-15 15:48:59 -0600

    Update expenses controllers

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,13 +1,65 @@
 class ExpensesController < ApplicationController
+  # GET /expenses or /expenses.json
   def index
+    @group_id = Group.find(params[:group_id])
+    @expenses = Expense.where(group_id: @group_id.id).order(created_at: :desc)
   end
 
-  def show
-  end
+  # GET /expenses/1 or /expenses/1.json
+  def show; end
 
+  # GET /expenses/new
   def new
+    @group_id = Group.find(params[:group_id]).id
+    @expense = Expense.new
   end
 
-  def edit
+  # GET /expenses/1/edit
+  def edit; end
+
+  # POST /expenses or /expenses.json
+  def create
+    @expense = Expense.new(expense_params)
+    @expense.user_id = current_user.id
+    @group = Group.find(params[:group_id])
+
+    @expense.group_id = @group.id
+
+    if @expense.save
+      redirect_to user_group_expenses_url(user_id: current_user.id, group_id: @group.id)
+    else
+      render :new
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @expense.update(expense_params)
+        format.html { redirect_to expense_url(@expense), notice: 'expense was successfully updated.' }
+        format.json { render :show, status: :ok, location: @expense }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @expense.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @expense.destroy
+
+    respond_to do |format|
+      format.html { redirect_to expenses_url, notice: 'expense successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_expense
+    @expense = Expense.find(params[:id])
+  end
+
+  def expense_params
+    params.require(:expense).permit(:name, :amount)
   end
 end
